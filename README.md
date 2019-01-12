cb1564a556221726387683c99204b1118b976695
Admin <Admin@Admins-MBP.fritz.box>
2019-01-12 12:20:08 +0100

    Remove unnecessary comments, create README.md, add years feature

---
+++ README.md
@@ -0,0 +1,1 @@
+Express App that searches the OMDb API for the movie you input. Returns titles and release years of movies that match your query.
--- app.js
+++ app.js
@@ -1,34 +1,20 @@
 
 var express = require('express');
-var app = express();
-app.use(express.static("public"));
-
-// for making http requests with javascript
 var request = require('request');
-
-// for making templates
 var ejs = require('ejs');
-// for not having to write .ejs every time
+
+var app = express();
+
+app.use(express.static("public"));
 app.set('view engine', 'ejs');
 
-app.get('/', function(req, res){
-	// search has been inputted and stored in the search.ejs file, whose form tag then called the / url and passed it here
-	var query = req.query.search;
-	// put it into the actual api search url and store that in a variable
+app.get('/', function(req, res){	
+	var query = req.query.search;	
 	var url = 'http://www.omdbapi.com/?s=' + query + '&apikey=thewdb';
-	// call the api with url, which contains the link with the search term
 	request(url, function(error, response, body){
-		// not mandatory, but good to have in case of error
 		if(!error && response.statusCode == 200){
-			// JSON.parse is converts the responding string (of JSON) into actual JSON
 			var data = JSON.parse(body);
-			// return the template results.ejs, passing the data on to it
 			res.render('results', {data: data});
-
-			// or directly return the entire json data:
-			// res.send(data);
-			// or specify it further:
-			//res.send(data.Search[0].Title);
 		};
 	});
 });
@@ -36,7 +22,3 @@ app.get('/', function(req, res){
 app.listen(process.env.PORT || 5000, function(){
 	console.log('works');
 });
-
-
-// General search: http://www.omdbapi.com/?s=guardians+of+the+galaxy&apikey=thewdb 
-// Search with Movie ID: http://www.omdbapi.com/?i=tt3896198&apikey=thewdb 
